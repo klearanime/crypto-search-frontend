c4e27d9e6841b4880e380f3972d2e82e9330c744
Crystal K Young <crystal.young@codeimmersive.com>
2021-04-14 22:44:02 -0400

    signup-login-home css/html

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'
-
+import Navbar from "./component/Navbar/Navbar"
 import Home from "./component/Home"
 import Login from "./component/Login"
-import SignUp from "./component/SignUp"
+import SignUp from "./component/SignUp/SignUp"
 
 
 const MainRouter = () => {
     return (
         <Router>
-            <nav>
-                <NavLink exact activeStyle={{ color: "red" }} to="/">Home</NavLink>
-                <NavLink activeStyle={{ color: "red" }} to="/sign-up">Sign Up</NavLink>
-                <NavLink activeStyle={{ color: "red" }} to="/login">Login</NavLink>
-            </nav>
+            <Navbar />
             <Switch>
                 <Route path="/sign-up" component={SignUp} />
                 <Route path="/login" component={Login} />
